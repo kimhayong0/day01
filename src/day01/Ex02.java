311ccb9ac70d92b45993597c63a397eca7b693d9
kimhayong0 <wlfhdn@hanmail.net>
2021-07-23 11:31:49 +0900

    Ex02 생성

---
+++ src/day01/Ex02.java
@@ -0,0 +1,11 @@
+package day01;
+
+public class Ex02 {
+	public static void main(String[] args) {
+		/*
+		 \n : 엔터와비슷	\t: 탭크기만큼우측이동
+		 \" : "(따옴표)표시(일반문자로사용하자할때)
+		 \\ : \ 문자로표현할때	  
+		 */
+	}
+}
