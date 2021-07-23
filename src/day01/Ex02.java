34bcf64406cf5da2532b203cb65ef1d6672afac8
kimhayong0 <wlfhdn@hanmail.net>
2021-07-23 12:49:10 +0900

    \관련

--- src/day01/Ex02.java
+++ src/day01/Ex02.java
@@ -3,9 +3,14 @@ package day01;
 public class Ex02 {
 	public static void main(String[] args) {
 		/*
-		 \n : 엔터와비슷	\t: 탭크기만큼우측이동
-		 \" : "(따옴표)표시(일반문자로사용하자할때)
+		 \n : 엔터와비슷	
+		 \t: 탭크기만큼우측이동 (윈도우에선 8칸의공간을 두고 띄워정렬)
+		 \": "(따옴표)표시(일반문자로사용하자할때)
 		 \\ : \ 문자로표현할때	  
 		 */
+		// " " : 쌍따옴표로 묶인 값은 문자열로 인식한다
+		System.out.println("H\ne\nl\nl\no java");
+		System.out.println("1\t1234\t4567\t89");
+		System.out.println("test \"123\" \\456");
 	}
 }
