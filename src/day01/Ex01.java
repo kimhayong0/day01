a132916704b344574287d9b2c63c911b4317552d
kimhayong0 <wlfhdn@hanmail.net>
2021-07-23 11:19:15 +0900

    주석,인쇄

--- src/day01/Ex01.java
+++ src/day01/Ex01.java
@@ -2,7 +2,11 @@ package day01;
 
 public class Ex01 {
 	public static void main(String[] args) {
-		System.out.println("존나졸림시발");
-		System.out.println("잠좀제때자자..");
+		/*문단주석
+		//System.out.println("존나졸림시발");
+		System.out.println("잠좀제때자자..");*/
+		
+		System.out.print("안녕하세요");
+		System.out.print("자바 시간입니다");
 	}
 }
