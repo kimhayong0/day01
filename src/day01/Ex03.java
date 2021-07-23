b2c99eb8f9f55437a142b76113c87f8df330c0bc
kimhayong0 <wlfhdn@hanmail.net>
2021-07-23 13:02:30 +0900

    숫자+문자

--- src/day01/Ex03.java
+++ src/day01/Ex03.java
@@ -1,5 +1,13 @@
 package day01;
 
 public class Ex03 {
-
-}
+	public static void main(String[] args) {
+		System.out.println(100+200);
+		System.out.println("Áß·Â"+500+"¹è");
+			//"º×´Ùºö30000"+10000="º×´Ùºö3000010000"
+		System.out.println("º×´Ùºö"+30000+10000);
+		System.out.println("º×´Ùºö¶±¶ô"+(30000-10000));
+		System.out.println("100"+100);
+				
+	}
+	}
